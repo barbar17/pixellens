d60cfc3c0d64cdecbf482a3080d6385af94d51b1
akbar <akbarmaulana644@gmail.com>
2026-03-10 16:48:33 +0700

    minor update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-slate-400">
-      HOME
+    <div className="min-h-screen min-w-full bg-slate-400">
+      <div className="w-full h-dvh overflow-hidden bg-[url(/hero1.png)]">
+        {/* <Image src={"/hero1.png"} width={1920} height={1080} className="w-full object-cover" alt="hero"/> */}
+      </div>
     </div>
   );
 }
