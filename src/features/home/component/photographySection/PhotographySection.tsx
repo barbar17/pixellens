54cfe1f7555df202d04526e2a97b8c851f1cb44f
barbar17 <akbarmaulana644@gmail.com>
2026-04-17 20:24:56 +0700

    fix minor bug

--- src/features/home/component/photographySection/PhotographySection.tsx
+++ src/features/home/component/photographySection/PhotographySection.tsx
@@ -23,7 +23,7 @@ const PhotographySection = () => {
         {
           PACKAGES.map((photo, index) => {
             if (index < 4) return (
-              <PhotographyCard key={photo.alt} src={photo.portfolio[0]} alt={photo.alt} title={photo.name} index={String(index + 1).padStart(2, "0")} href={photo.href} />
+              <PhotographyCard key={photo.alt} src={photo.portofolio[0]} alt={photo.alt} title={photo.name} index={String(index + 1).padStart(2, "0")} href={photo.href} />
             )
           })
         }
